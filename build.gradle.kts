cc1b0c7374767d12b55006416122deccb7cb9212
Brian Altan <brianaltan28@gmail.com>
2025-02-21 12:09:46 +0700

    feat: add sonarcloud cs

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,10 +3,18 @@ plugins {
     jacoco
     id("org.springframework.boot") version "3.4.2"
     id("io.spring.dependency-management") version "1.1.7"
+    id ("org.sonarqube") version "6.0.1.5171"
 }
 
 group = "id.ac.ui.cs.advprog"
 version = "0.0.1-SNAPSHOT"
+
+sonar {
+    properties {
+        property "sonar.projectKey", "adpro-eshop"
+        property "sonar.projectName", "adpro-eshop"
+    }
+}
 
 java {
     toolchain {
