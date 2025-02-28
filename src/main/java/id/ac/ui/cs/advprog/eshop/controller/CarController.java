7c91c16bdcee89ae666376a3564bd10ab87a1551
Brian Altan <brianaltan28@gmail.com>
2025-02-28 12:41:06 +0700

    refactor: carcontroller with dip principle

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/CarController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/CarController.java
@@ -1,8 +1,7 @@
 package id.ac.ui.cs.advprog.eshop.controller;
 
 import id.ac.ui.cs.advprog.eshop.model.Car;
-import id.ac.ui.cs.advprog.eshop.service.CarServiceImpl;
-import id.ac.ui.cs.advprog.eshop.service.ProductService;
+import id.ac.ui.cs.advprog.eshop.service.CarService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,10 +12,10 @@ import java.util.List;
 @Controller
 @RequestMapping("/car")
 class CarController {
-    private final CarServiceImpl carservice;
+    private final CarService carservice;
 
     @Autowired
-    public CarController(CarServiceImpl carservice) {
+    public CarController(CarService carservice) {
         this.carservice = carservice;
     }
 
